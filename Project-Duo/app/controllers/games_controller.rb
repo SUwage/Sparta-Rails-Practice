ba47b9e65935cbb99a6a3fa1ca4019cfead175e6
unknown <suwage@spartaglobal.com>
2018-07-26 10:32:42 +0100

    Game/User model working, Games/User controller working. Does not have sign-out button

---
+++ Project-Duo/app/controllers/games_controller.rb
@@ -0,0 +1,57 @@
+class GamesController < ApplicationController
+  before_action :set_game, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+
+  def index
+    @games = current_user.games
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    @game.user = current_user
+    respond_to do |format|
+      if @game.save
+        format.html { redirect_to @game, notice: 'Game was successfully created.' }
+        format.json { render :show, status: :created, location: @game }
+      else
+        format.html { render :new }
+        format.json { render json: @game.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @game.update(game_params)
+        format.html { redirect_to @game, notice: 'Game was successfully updated.' }
+        format.json { render :show, status: :ok, location: @game }
+      else
+        format.html { render :edit }
+        format.json { render json: @game.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @game.destroy
+    respond_to do |format|
+      format.html { redirect_to games_url, notice: 'Game was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_game
+      @game = Game.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def game_params
+      params.require(:game).permit(:title, :description)
+    end
+end
--- Project-Duo/config/routes.rb
+++ Project-Duo/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'games/index'
+  # get 'games/new'
+  # get 'games/show'
+  # get 'games/edit'
   devise_for :users
+  resources :games
+
+  root 'games#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
